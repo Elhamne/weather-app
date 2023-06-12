d5e5a248f46dc73d071581a18104052eb3442fcc
Elhamne <elham.ne94@gmail.com>
2023-06-12 17:19:53 +0330

    Weather project added

---
+++ src/index.js
@@ -0,0 +1,56 @@
+const apiKey = 'b70ee57c3c80ad353268cc880561796d';
+let units = 'metric';
+
+const capitalize = (string) => {
+  string = string.toLowerCase().trim();
+  return string.charAt(0).toUpperCase() + string.slice(1);
+};
+
+const showTemprature = (response) => {
+  console.log(response.data);
+  let tempElement = document.querySelector('#temp');
+  let humidityElement = document.querySelector('#humidity');
+  let windElement = document.querySelector('#wind');
+  let header = document.querySelector('h1');
+  let descElement = document.querySelector('#description');
+
+  let city = response.data.name;
+  let temp = Math.round(response.data.main.temp);
+  let humidity = response.data.main.humidity;
+  let wind = response.data.wind.speed;
+  let description = response.data.weather[0].description;
+
+  header.innerHTML = city;
+  descElement.innerHTML = description;
+  tempElement.innerHTML = temp;
+  humidityElement.innerHTML = humidity;
+  windElement.innerHTML = wind;
+};
+
+const showCityWeather = (e) => {
+  e.preventDefault();
+  let searchCity = capitalize(document.querySelector('#search-value').value);
+
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchCity}&units=${units}&appid=${apiKey}`;
+
+  axios.get(apiUrl).then(showTemprature);
+};
+
+const showCurrentWeather = (e) => {
+  e.preventDefault();
+
+  navigator.geolocation.getCurrentPosition((position) => {
+    let lat = position.coords.latitude;
+    let lon = position.coords.longitude;
+
+    let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=${units}&appid=${apiKey}`;
+
+    axios.get(apiUrl).then(showTemprature);
+  });
+};
+
+let searchForm = document.querySelector('#search-form');
+searchForm.addEventListener('submit', showCityWeather);
+
+let currentBtn = document.querySelector('#current');
+currentBtn.addEventListener('click', showCurrentWeather);
