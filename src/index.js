528884d7036e64e5f497cda2fa52c869bd6ffad2
Elhamne <elham.ne94@gmail.com>
2023-06-13 23:27:59 +0330

    Units change added

--- src/index.js
+++ src/index.js
@@ -1,6 +1,8 @@
 const apiKey = 'b70ee57c3c80ad353268cc880561796d';
 let units = 'metric';
 let city = 'tehran';
+let temp = 0;
+let isCelsius = true;
 
 const capitalize = (string) => {
   string = string.toLowerCase().trim();
@@ -15,7 +17,7 @@ const showTemprature = (response) => {
   let descElement = document.querySelector('#description');
 
   let city = response.data.name;
-  let temp = Math.round(response.data.main.temp);
+  temp = Math.round(response.data.main.temp);
   let humidity = response.data.main.humidity;
   let wind = response.data.wind.speed;
   let description = response.data.weather[0].description;
@@ -34,11 +36,54 @@ const search = (cityName) => {
 
 const handleSumbit = (e) => {
   e.preventDefault();
+  isCelsius = true;
+  celsius.classList.remove('active');
+  fahrenheit.classList.add('active');
   let searchCity = capitalize(document.querySelector('#search-value').value);
   search(searchCity);
 };
 
-search(city);
+const celsiusToFahrenheit = (c) => {
+  let f = Math.round(c * (9 / 5) + 32);
+  return f;
+};
+
+const fahrenheitToCelsius = (f) => {
+  let c = Math.round(((f - 32) * 5) / 9);
+  return c;
+};
+
+const changeToFahrenheit = () => {
+  let tempElement = document.querySelector('#temp');
+
+  if (isCelsius) {
+    tempElement.innerHTML = celsiusToFahrenheit(temp);
+    temp = celsiusToFahrenheit(temp);
+    isCelsius = false;
+    fahrenheit.classList.remove('active');
+    celsius.classList.add('active');
+  } else return;
+};
+
+const changeToCelsius = () => {
+  let tempElement = document.querySelector('#temp');
+
+  if (!isCelsius) {
+    tempElement.innerHTML = fahrenheitToCelsius(temp);
+    temp = fahrenheitToCelsius(temp);
+    isCelsius = true;
+    celsius.classList.remove('active');
+    fahrenheit.classList.add('active');
+  } else return;
+};
+
+let celsius = document.querySelector('#celsius');
+let fahrenheit = document.querySelector('#fahrenheit');
+
+fahrenheit.addEventListener('click', changeToFahrenheit);
+celsius.addEventListener('click', changeToCelsius);
 
 let searchForm = document.querySelector('#search-form');
 searchForm.addEventListener('submit', handleSumbit);
+
+search(city);
